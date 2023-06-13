7e15a91edeeed4b45f7e7519f7c3d16a8460285d
obaidul-islam <obaidul15-2255@diu.edu.bd>
2023-06-13 20:13:33 +0600

    add function

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
 
 class UserController extends Controller
 {
-    public function store(){
+    public function store(Request $request){
+        $input = $request->all();
 
+        $validation = Validator::make($input,[
+            'name' => 'required',
+            'email' => 'required|unique:user,email|email',
+            'password' => 'required|min:6',
+        ]);
 
+        if($validation->fails()){
+
+        }
     }
 }
